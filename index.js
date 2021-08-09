39944f2fcc4763a3b257f3d345c6131695ea96b6
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2021-08-09 14:07:01 -0400

    added new animations to landing

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
     console.log("ready!");
+
+    let timelineLanding = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.landing__wrapper',
+
+        },
+    });
+
+    timelineLanding
+        .from(".landing-img", { opacity: 0, duration: .5 }, "-1")
+        .from(".landing-text__company", { opacity: 0, duration: .5 },)
+        .from(".landing-text__animation", { opacity: 0, duration: .5 })
+        .from(".landing__btn", { opacity: 0, duration: .5 })
+
+
     // =================== Solutions Animation ==================== //
 
     let timelineSolutions = gsap.timeline({
