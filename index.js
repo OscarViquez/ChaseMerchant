da807eb31d8d96e3512816c437ba1686348a452a
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2021-08-09 13:31:51 -0400

    finished layout for landing, and soution section. Made the site responsive. added GSAP animations

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    console.log("ready!");
+
+    let timelineLanding = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.landing__wrapper',
+
+        },
+    });
+
+    timelineLanding
+        .from(".landing-img__container", { opacity: 0, duration: .5 }, "-1")
+        .from(".landing-text__company", { opacity: 0, duration: .5 },)
+        .from(".landing-text__animation", { x: 20, opacity: 0, duration: .5 })
+        .from(".landing__btn", { opacity: 0, duration: .5 })
+
+
+    // =================== Solutions Animation ==================== //
+
+    let timelineSolutions = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.solution__wrapper',
+
+        },
+    });
+
+    timelineSolutions
+        .from(".solution-p", { opacity: 0, duration: 1 }, "-1")
+        .from(".solution-card ", { opacity: 0, duration: .5 },)
+
+});
